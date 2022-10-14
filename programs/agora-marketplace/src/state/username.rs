c262fd955445d793736ac4ecd396a0cb3623a72f
amukh621 <68251719+amukh621@users.noreply.github.com>
2022-10-13 21:11:47 -0500

    Committed

--- programs/agora-marketplace/src/state/username.rs
+++ programs/agora-marketplace/src/state/username.rs
@@ -3,6 +3,16 @@ use anchor_lang::prelude::*;
 #[account]
 pub struct UserAccount { //userKey
     //seed: username
+    user: Pubkey
+}
+
+impl UserAccount{
+    pub const STATIC_SIZE: usize = 8 + 32;
+
+    pub fn initialize(&mut self, pubkey: Pubkey) -> Result<()> {
+        self.user = pubkey;
+        Ok(())
+    }
 }
 
 //random change
